235de67735a7b518697dbd9c7b03a3eab165a83c
Kingsane <157904333+Aliansyah20@users.noreply.github.com>
2026-09-11 14:07:30 +0700

    Create seed_users.py

---
+++ seed_users.py
@@ -0,0 +1,21 @@
+from app import create_app
+from app.extensions import db
+from app.models import User
+from werkzeug.security import generate_password_hash
+
+app = create_app()
+
+with app.app_context():
+    # Cek apakah user sudah ada agar tidak dobel
+    if not User.query.filter_by(username='sekben1').first():
+        sekben = User(
+            nama_lengkap="Siti Sekben",
+            username="sekben1",
+            password=generate_password_hash("rahasia123"),
+            role="sekben"
+        )
+        db.session.add(sekben)
+        db.session.commit()
+        print("Berhasil! Akun Sekben dibuat. Username: sekben1 | Pass: rahasia123")
+    else:
+        print("Akun sekben1 sudah ada di database.")
