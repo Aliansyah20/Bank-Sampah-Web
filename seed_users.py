bb84acccbef2546cd0e6aee8f24b10bc35397655
Kingsane <157904333+Aliansyah20@users.noreply.github.com>
2026-09-13 09:02:25 +0700

    Update seed_users.py

--- seed_users.py
+++ seed_users.py
@@ -19,4 +19,3 @@ with app.app_context():
         print("Berhasil! Akun Sekben dibuat. Username: sekben1 | Pass: rahasia123")
     else:
         print("Akun sekben1 sudah ada di database.")
-        print("Verivikasi akun")
